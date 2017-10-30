829abd2adc6b04e2dba33cca13a12b5a522c0c6c
h2andp <h2andp@example.com>
2017-10-30 17:16:22 +0200

    Initial structure import

---
+++ src/main/kotlin/net/h2andp/Application.kt
@@ -0,0 +1,11 @@
+package net.h2andp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
